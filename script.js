13830774d2dd19a320c138f196b0335bf6c8efec
imo-jpg <imo.todd@gmail.com>
2023-03-10 16:25:07 -0500

    makes mobile enhancements

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ function addFilmEdges() {
     for (let i = 0; i < edges[0].length; i++) {
         let stats = edges[0][i].getBoundingClientRect();
         
-        for (let j = 0; j < stats.height/35; j++) {
-            let div = document.createElement("div");
-            div.className = "film_gap";
-            edges[0][i].append(div);
+       for (let j = 0; j < stats.height/35; j++) {
+                let div = document.createElement("div");
+                div.className = "film_gap";
+                edges[0][i].append(div);
         }
+        
     };
 };
 
